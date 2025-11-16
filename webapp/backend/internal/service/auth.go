828daa87b132b6891535c2c932a37212d9eedf90
SakaueYae <sky.bgm.tdrksht.132133@gmail.com>
2025-11-16 13:04:26 +0900

    delete: 一部ログを削除

--- webapp/backend/internal/service/auth.go
+++ webapp/backend/internal/service/auth.go
@@ -70,6 +70,6 @@ func (s *AuthService) Login(ctx context.Context, userName, password string) (str
 	if err != nil {
 		return "", time.Time{}, err
 	}
-	log.Printf("Login successful for UserName '%s', session created.", userName)
+	// log.Printf("Login successful for UserName '%s', session created.", userName)
 	return sessionID, expiresAt, nil
 }
